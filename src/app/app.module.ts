f3317f363c9274bf3110170c323a33ac6de0c969
Yonis7 <yonisaden7@gmail.com>
2024-03-30 16:56:21 -0400

    Add global styles and import material themes*** ***Update index.html with font and icon links*** ***Add routing for default and dashboard components*** ***Import default module and enable animations*** ***Update dependencies in package.json

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DefaultModule } from './layouts/default/default.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    DefaultModule
   ],
   providers: [],
   bootstrap: [AppComponent]
